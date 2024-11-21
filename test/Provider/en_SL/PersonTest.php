98004f26188c588706a416ec6cad735312ba2817
avgkudey <avgk.udey@gmail.com>
2024-11-21 20:39:41 +0300

    Sri Lanka Provider Person and Phone Number

---
+++ test/Provider/en_SL/PersonTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Faker\Test\Provider\en_SL;
+
+use Faker\Provider\en_SL\Person;
+use Faker\Test\TestCase;
+
+final class PersonTest extends TestCase
+{
+    public function testNicNumberOld(): void
+    {
+        for ($i = 0; $i < 100; ++$i) {
+            $nic = $this->faker->nicNumberOld;
+
+            self::assertSame(10, strlen($nic));
+            self::assertMatchesRegularExpression('/^\d{9}[VX]$/', $nic);
+        }
+    }
+
+    public function testMaleName(): void
+    {
+        for ($i = 0; $i < 100; ++$i) {
+            $name = $this->faker->name('male');
+
+            self::assertGreaterThan(0, strlen($name));
+        }
+    }
+    public function testFeMaleName(): void
+    {
+        for ($i = 0; $i < 100; ++$i) {
+            $name = $this->faker->name('female');
+
+            self::assertGreaterThan(0, strlen($name));
+        }
+    }
+
+    public function testNicNumber(): void
+    {
+        for ($i = 0; $i < 100; ++$i) {
+            $nic = $this->faker->nicNumber;
+
+            self::assertSame(12, strlen($nic), "NIC number should be 12 characters long: $nic");
+
+            self::assertTrue(in_array($nic[0], ['9', '0'], true));
+
+            $year = (int) substr($nic, 1, 4);
+            self::assertGreaterThanOrEqual(1900, $year);
+            self::assertLessThanOrEqual((int) date('Y'), $year);
+
+            $midPart = substr($nic, 5, 5);
+            self::assertMatchesRegularExpression('/^\d{5}$/', $midPart);
+
+            $lastPart = substr($nic, 10, 1);
+            self::assertMatchesRegularExpression('/^\d$/', $lastPart);
+        }
+    }
+
+    protected function getProviders(): iterable
+    {
+        yield new Person($this->faker);
+    }
+}
